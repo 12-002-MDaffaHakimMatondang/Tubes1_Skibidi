5f6443b928d5b0bcef7ee31b25f2c7ac349a4f68
12-002-MDaffaHakimMatondang <muhammad.123140002@student.itera.ac.id>
2025-06-01 21:52:27 +0700

    Update run-bots.sh

--- run-bots.sh
+++ run-bots.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-python main.py --logic Sifu --email=test@email.com --name=stima --password=123456 --team etimo &
-python main.py --logic Sifu --email=test1@email.com --name=stima1 --password=123456 --team etimo &
-python main.py --logic Sifu --email=test2@email.com --name=stima2 --password=123456 --team etimo &
-python main.py --logic Sifu --email=test3@email.com --name=stima4 --password=123456 --team etimo &
+python main.py --logic Sifu --email=test@email.com --name=stima2 --password=123458 --team etimo &
+python main.py --logic Sifu --email=test1@email.com --name=stima4 --password=123460 --team etimo &
+python main.py --logic Master --email=test2@email.com --name=stima13 --password=123460 --team etimo &
+python main.py --logic Master --email=test3@email.com --name=stima6 --password=123460 --team etimo &
+
+
